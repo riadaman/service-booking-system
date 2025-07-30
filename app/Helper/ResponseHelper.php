e9206671766e5ae0b3576731e31e642c80ca9884
riadaman <riadmohammod2000@gmail.com>
2025-07-30 11:44:34 +0600

    add: Response Helper function for all api response

---
+++ app/Helper/ResponseHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helper;
+
+class ResponseHelper
+{
+    public static function success($data, $message = '', $status = 'success', $code = 200)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public static function error($message = '', $status = 'error', $code = 400)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+        ], $code);
+    }
+}
